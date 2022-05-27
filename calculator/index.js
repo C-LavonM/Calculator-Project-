38233c107bc4d53ce95e67555aaef2280e104869
Lavon <lavon_c@outlook.com>
2022-05-27 00:19:42 -0400

    commit

--- calculator/index.js
+++ calculator/index.js
@@ -1,21 +1,35 @@
-function calc() {
-    var a = parseInt(document.querySelector("#value1").value);
-    var b = parseInt(document.querySelector("#value2").value);
-    var op = document.querySelector("#operator").value;
-    var calculate;
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
 
-    if (op == "add") {
-        calculate = a + b;
-    } else if (op == "min") {
-        calculate = a - b;
-    } else if (op == "div") {
-        calculate = a / b;
-    } else if (op == "mul") {
-        calculate = a * b;
-    }
-    document.querySelector("#result").innerHTML = calculate;
+buttons.forEach((item) => {
+    item.onclick = () => {
+        if (item.id == "clear") {
+            display.innerText = "";
+        } else if (item.id == "backspace") {
+            let string = display.innerText.toString();
+            display.innerText = string(0, string.length - 1);
+        } else if (display.innerText != "" && item.id == "equal") {
+            display.innerText = eval(display.innerText);
+        } else if (display.innerText == "" && item.id == "equal") {
+           display.innerText = "Empty!";
+           setTimeout(() => (display.innerText = ""), 2000);
+        } else {
+            display.innerText += item.id;
+        }
+    }; 
+});
 
-}
+//Dark theme//
+
+const themeToggleBtn = document.querySelector(".theme-toggler");
+const calculator = document.querySelector(".calculator");
+const toggleIcon = document.querySelector(".toggler-icon");
+let isDark = true;
+themeToggleBtn.onclick = () => {
+    calculator.classList.toggle("dark");
+    themeToggleBtn.classList.toggle("active");
+    isDark = !isDark;
+};
 
 
 
